142bc64c934a7279140e87a470a10aa297a3e8b5
Matt Hartley <mbhartley1490@gmail.com>
2014-02-04 09:51:24 -0500

    cleaned up code and re-set strategy

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,44 +7,8 @@ $('.reg-tab').click (function(){
 
 $('.registration-submit').click (function(){
 	$('.registrationForm').toggle();
+	
 })
-
-//$ ("registration-submit").click(function() {
-//	var nameValue = $('input[name=Name]').val();
-//	var mailAddressValue = $('input[name=Phone]').val();
-//	var cityValue = $('input[name=Email]').val();
-//	var stateValue = $('input[name=City]').val();
-//	var zipCodeValue = $('input[name=State]').val();
-//	var emailValue = $('input[name=Zip]').val();
-//	var vehicleYearValue = $('input[name=Year]').val();
-//	var vehicleMakeValue = $('input[name=Make]').val();
-//	var vehicleModelValue = $('input[name=Model]').val();
-//	var vehiclePhoto = $('input[name=Image]').val();
-
-//var registrant = new Registrant(name, mailAddress, city, state, zipCode, email, vehicleYear, vehicleMake, vehicleModel)
-
-//var RegistrantTemplate = _.template ($('.RegistrantTemplate').text());
-
-//$('.registered-users').append(RegistrantTemplate({data: registrant}))
-
-//})
-
-
-
-
-
-
-//function RegistrationBuild (name, mailAddress, city, state, zipCode, email, vehicleYear, vehicleMake, vehicleModel) {
-//  this.name = name;
-//  this.mailAddress = mailAddress;
-//  this.city = city;
-//  this.state = state;
-//  this.zipCode = zipCode;
-//  this.email = email;
-//  this.vehicleYear = vehicleYear;
-//  this.vehicleMake = vehicleMake;
-//  this.vehicleModel = vehicleModel;
-//}
 
 
 
