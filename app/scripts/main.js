eeca81b376bb40a98217ee4b0208a221bc99b168
Matt Hartley <mbhartley1490@gmail.com>
2014-02-03 20:36:21 -0500

    stopped, to install underscore

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,6 +9,26 @@ $('.registration-submit').click (function(){
 	$('.registration-form').toggle();
 })
 
+$('.registration-submit').click(function(){
+
+})
+
+function RegistrationBuild (name, address, city, state, zipCode, email, vehicleYear, vehicleMake, vehicleModel) {
+	this.name - name;
+	this.address = address;
+	this.city = city;
+	this.state = state;
+	this.zipCode = zipCode;
+	this.email = email;
+	this.vehicleYear = vehicleYear;
+	this.vehicleMake = vehicleMake;
+	this.vehicleModel = vehicleModel;
+}
+
+$('.registration-submit').click(function(){
+	var newDiv = $('<div class = "reg-name">' + RegistrationBuild.name + '</div>');
+})
+
 
 
  
